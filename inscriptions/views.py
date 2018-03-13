2b951273f6621614eba59c8331be0b48c6b8477d
Puyb <puyb@puyb.net>
2018-03-13 11:19:45 +0100

    tarif mars/mai

--- inscriptions/views.py
+++ inscriptions/views.py
@@ -70,6 +70,10 @@ def inscription(request):
                 new_instance.prix = Decimal(140)
             if new_instance.autre_club:
                 new_instance.prix -= Decimal(35)
+            if date.today() >= date(date.today().year, 3, 1):
+                new_instance.prix -= Decimal(20)
+            if date.today() >= date(date.today().year, 5, 1):
+                new_instance.prix -= Decimal(20)
             
             new_instance.saison = saison
             new_instance.save()
